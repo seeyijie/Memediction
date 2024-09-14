58d43b1af8f172ab9a01018fbd89d25efec08701
Matthew <alexandermatthew12@gmail.com>
2024-09-15 02:46:33 +0800

    chore: modify script to check if instance exists

--- setup-local.sh
+++ setup-local.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
-# Background task
-anvil &
+check_anvil_running() {
+  if lsof -i:8545 >/dev/null; then
+    return 0
+  else
+    return 1
+  fi
+}
 
-# Save the PID of the anvil process
-ANVIL_PID=$!
-
-# Wait for 3 seconds
-sleep 3
-
-# Check if the blockchain is running using `cast`
-RPC_URL="http://127.0.0.1:8545"
-BLOCK_CHECK=$(cast block --rpc-url $RPC_URL)
-
-if [[ $BLOCK_CHECK == *"error"* ]]; then
-  echo "Failed to connect to Anvil. Exiting..."
-  # Kill the anvil process
-  kill $ANVIL_PID
-  exit 1
+if check_anvil_running; then
+  echo "Anvil is already running. Skipping Anvil startup..."
 else
-  echo "Anvil is running. Proceeding with the script..."
+  echo "Starting Anvil..."
+
+  # Run anvil in the background
+  anvil &
+
+  ANVIL_PID=$!
+
+  sleep 3
+
+  RPC_URL="http://127.0.0.1:8545"
+  BLOCK_CHECK=$(cast block --rpc-url $RPC_URL)
+
+  if [[ $BLOCK_CHECK == *"error"* ]]; then
+    echo "Failed to connect to Anvil. Exiting..."
+    # Kill the anvil process if started in this script
+    kill $ANVIL_PID
+    exit 1
+  else
+    echo "Anvil is running. Proceeding with the script..."
+  fi
 fi
+
 
 # Deploy the contracts, setup the hooks and start initializing
 forge script script/V4Deployer.s.sol:V4Deployer --rpc-url http://127.0.0.1:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --broadcast && forge script script/HookMiningSample.s.sol:HookMiningSample --rpc-url http://127.0.0.1:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --broadcast
