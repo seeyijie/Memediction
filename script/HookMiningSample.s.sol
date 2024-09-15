45618790c702b9ae63d2e28439b44c806f8e0e92
Matthew <alexandermatthew12@gmail.com>
2024-09-15 15:33:41 +0800

    fix: deployment script for hook address permissions

--- script/HookMiningSample.s.sol
+++ script/HookMiningSample.s.sol
@@ -51,7 +51,10 @@ contract HookMiningSample is Script {
         usdmToken.mint(USER_A, 1000e18);
         console.log("Balance of user A:", usdmToken.balanceOf(USER_A));
 
-        uint160 flags = uint160(Hooks.BEFORE_INITIALIZE_FLAG | Hooks.BEFORE_SWAP_FLAG);
+        uint160 flags =  uint160(
+            Hooks.BEFORE_INITIALIZE_FLAG | Hooks.BEFORE_SWAP_FLAG | Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG
+            | Hooks.BEFORE_ADD_LIQUIDITY_FLAG | Hooks.AFTER_SWAP_FLAG
+        );
 
         address CREATE2_DEPLOYER = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
         (address hookAddress, bytes32 salt) =
